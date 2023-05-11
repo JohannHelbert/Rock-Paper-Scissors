0235ef8f7324cd9f3cd27548d0fddf1572ae0002
JohannHelbert <>
2023-05-11 07:33:30 +0000

    Add event listener to all buttons

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
