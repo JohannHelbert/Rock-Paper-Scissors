6d42dd7c8009b03c3057dc8aae433229129cbeee
JohannHelbert <>
2023-05-11 08:05:43 +0000

    Rename images

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,18 @@ for (let button of buttons) {
         playGame(playerChoice);
     });
 }
+
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() *3);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
